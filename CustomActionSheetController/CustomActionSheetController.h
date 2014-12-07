284396ce7ce05d5ca150b0f24586556075da5fe1
tokorom <tokorom@gmail.com>
2014-12-07 22:39:58 +0900

    First commit.

---
+++ CustomActionSheetController/CustomActionSheetController.h
@@ -0,0 +1,19 @@
+//
+//  CustomActionSheetController.h
+//  CustomActionSheetController
+//
+//  Created by tokorom on 12/7/14.
+//  Copyright (c) 2014 tokoro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomActionSheetController.
+FOUNDATION_EXPORT double CustomActionSheetControllerVersionNumber;
+
+//! Project version string for CustomActionSheetController.
+FOUNDATION_EXPORT const unsigned char CustomActionSheetControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomActionSheetController/PublicHeader.h>
+
+
---
+++ CustomActionSheetDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomActionSheetDemo
+//
+//  Created by tokorom on 12/7/14.
+//  Copyright (c) 2014 tokoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
